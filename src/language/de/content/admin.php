022909e5974d9b7dd31333ed34248dfdfb794201
Tropby <Tropby@elektronik2000.de>
2020-11-25 16:21:43 +0100

    Changed license header to LGPL

--- src/language/de/content/admin.php
+++ src/language/de/content/admin.php
@@ -1,17 +1,22 @@
 <?php 
 
-/***************************************************************\
-|                                                               |
-|                   apexx CMS & Portalsystem                    |
-|                 ============================                  |
-|           (c) Copyright 2005-2009, Christian Scheb            |
-|                  http://www.stylemotion.de                    |
-|                                                               |
-|---------------------------------------------------------------|
-| THIS SOFTWARE IS NOT FREE! MAKE SURE YOU OWN A VALID LICENSE! |
-| DO NOT REMOVE ANY COPYRIGHTS WITHOUT PERMISSION!              |
-| SOFTWARE BELONGS TO ITS AUTHORS!                              |
-\***************************************************************/
+/*
+	Open Apexx Module Manager
+	(c) Copyright 2005-2009, Christian Scheb
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 2.1 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #
 # German Language Pack
